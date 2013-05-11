3aad1cf09ca15ea3c1d8dbf7f037c0de7ff33c47
bilouro <bilouro@bilouro.com>
2013-05-11 15:15:50 -0300

    metodo preenche todos os horarios dos funcionarios para um mes.

--- cadastro/views.py
+++ cadastro/views.py
@@ -25,7 +25,36 @@ from django.contrib.auth.decorators import login_required
 #import re
 from django.conf import settings
 from util import utils
+from cadastro.models import *
 
 @login_required
 def index(request):
     return render_to_response('index.html', {'user':request.user})
+
+
+@login_required
+def cria_agenda(request):
+    import calendar, datetime
+
+    ANO=2013
+    MES=5
+
+    ult_dia_mes=calendar.monthrange(ANO,MES)[1]
+    d = datetime.date(ANO,MES,1)
+    dias_list = range(ult_dia_mes)
+
+    #todos ids que tem ao menos uma especialidade
+    id_funcionario_com_especialidade_list = EspecialidadeFuncionario.objects.all().values_list('funcionario__id', flat=True).distinct()
+    #busca os funcionarios
+    funcionario_com_especialidade_list = Funcionario.objects.filter(id__in= id_funcionario_com_especialidade_list)
+
+    #busca os horarios disponiveis
+    horario_disponivel_list = HorarioDisponivel.objects.all()
+
+    for h,f,soma in itertools.product(horario_disponivel_list, funcionario_com_especialidade_list, dias_list):
+       hdf = HorarioDisponivelFuncionario()
+       hdf.data = d + datetime.timedelta(days=soma)
+       hdf.hora = h
+       hdf.funcionario = f
+       hdf.disponivel = True
+       hdf.save()
