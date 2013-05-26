7c3969404d4e2590495147584f24fef0874156ad
bilouro <bilouro@bilouro.com>
2013-05-26 03:32:40 -0300

    addming funcionario report --- adicionando relatorio de funcionario

--- cadastro/cbv/relatoriofuncionarios.py
+++ cadastro/cbv/relatoriofuncionarios.py
@@ -1,7 +1,12 @@
 import urllib
+import datetime
 from django import forms
 #from django.core.urlresolvers import reverse
+from django.db.models.query_utils import Q
 from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.utils import timezone
 from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
 from cadastro.models import *
@@ -30,36 +35,35 @@ class RelatorioFuncionarioFiltro(FormView):
         status = forms.ModelMultipleChoiceField(queryset=StatusFuncionario.objects.all(), initial=[1,])
         cargo_list = Cargo.objects.all()
         cargo = forms.ModelMultipleChoiceField(queryset=cargo_list, initial=cargo_list)
+        imprime_filtro = forms.BooleanField(initial=True, required=False)
 
     template_name = 'cadastro/cbv/relatoriofuncionariosfiltro.html'
     form_class = BuscaForm
     success_url = '/cadastro/relatorio/funcionario/resultado'
 
     def form_valid(self, form):
-        valores = {}
-        valores['admissao_de'] = '' if form.cleaned_data['admissao_de'] == None else form.cleaned_data['admissao_de']
-        valores['admissao_ate'] = '' if form.cleaned_data['admissao_ate'] == None else form.cleaned_data['admissao_ate']
-        valores['aniversaria_em'] = '-'.join([ ae for ae in form.cleaned_data['aniversaria_em'] ])
-        valores['status'] = '-'.join([ str(s.pk) for s in form.cleaned_data['status'] ])
-        valores['cargo'] = '-'.join([ str(c.pk) for c in form.cleaned_data['cargo'] ])
+        funcionario_list = Funcionario.objects.all()
+        if form.cleaned_data['admissao_de']:
+            funcionario_list = funcionario_list.filter(data_admissao__gte=form.cleaned_data['admissao_de'])
+        if form.cleaned_data['admissao_ate']:
+            funcionario_list = funcionario_list.filter(data_admissao__lte=form.cleaned_data['admissao_ate'])
+        if form.cleaned_data['aniversaria_em']:
+            mes_aniversaria_em_list = []
+            for mes in form.cleaned_data['aniversaria_em']:
+                mes_aniversaria_em_list.append(Q(data_nascimento__month=mes))
+            if mes_aniversaria_em_list:
+                from operator import __or__ as OR
+                funcionario_list = funcionario_list.filter(reduce(OR, mes_aniversaria_em_list))
 
-        return HttpResponseRedirect(self.get_success_url() + '?' + urllib.urlencode(valores))
-#        return super(RelatorioFuncionarioFiltro, self).form_valid(form)
+        if form.cleaned_data['status']:
+            funcionario_list = funcionario_list.filter(status__in=form.cleaned_data['status'])
+        if form.cleaned_data['cargo']:
+            funcionario_list = funcionario_list.filter(cargo__in=form.cleaned_data['cargo'])
 
-class RelatorioFuncionarioResultado(ListView):
-    model = Funcionario
-    template_name = 'cadastro/cbv/relatoriofuncionariosresultado.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(RelatorioFuncionarioResultado, self).get_context_data(**kwargs)
-        return context
-
-    def dispatch(self, request, *args, **kwargs):
-        return super(RelatorioFuncionarioResultado, self).dispatch(request, *args, **kwargs)
-
-    def get_queryset(self):
-        queryset = super(RelatorioFuncionarioResultado, self).get_queryset()
-        return queryset
-
-    def get(self, request, *args, **kwargs):
-        return super(RelatorioFuncionarioResultado, self).get(request, *args, **kwargs)
+        return render_to_response('cadastro/cbv/relatoriofuncionariosresultado.html', {
+            'form': form,
+            'object_list':funcionario_list,
+            'now': timezone.datetime.now(),
+            'imprime_filtro': form.cleaned_data['imprime_filtro']
+        }, context_instance=RequestContext(self.request))
+        #return super(RelatorioFuncionarioFiltro, self).form_valid(form)
